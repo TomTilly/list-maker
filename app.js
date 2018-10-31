2019681966530152b274a641ead5e50e5285f450
Tom Tilly <tomtillistrand@gmail.com>
2018-10-31 16:16:22 -0400

    integrate ejs

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 7777;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+/* Routes */
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('*', function(req, res) {
+	res.send('Page not found');
+});
+
+app.listen(port,() => { 
+	console.log(`Server started on ${port}`);
+});
